9296ceba1bda5c730e69041474c1e72db9f29478
danielabramiani <danielabramiani4@gmail.com>
2023-12-23 03:08:20 +0400

    second commit

---
+++ home work/python hw/day8.py
@@ -0,0 +1,13 @@
+family=("Ninuca","Chiko","mariami","Danieli")
+
+full_sentence="MY moms name is:{}MY dads name is:{}My sister name is{}My name is:{}".format(family[0],family[1],family[2],family[3])
+print(full_sentence)
+
+family_ages=(32,33,9,13)
+
+full_sentence="My moms age is:{}My dads age is:{}My sister age is{}My name is:{}".format(family_ages[0],family_ages[1],family_ages[2],family_ages[3])
+print(full_sentence)
+
+family_ages=(32+10,33+10,9+10,13+10)
+
+full_sentence="My moms age after 10 years is:{}My dads age after 10 years is:{}My sister age after 10 years is:{}My age after 10 years is:{}".format(family_ages[0],family_ages[1],family_ages[2],family_ages[3])
