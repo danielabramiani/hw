4dd8142c3bf4ec559666741c2fb2515c4155fd9d
danielabramiani <danielabramiani4@gmail.com>
2023-12-29 15:47:12 +0400

    second commit

---
+++ ჩემი ნამუშევრები/learn.js
@@ -0,0 +1,35 @@
+// console.log("i like Adjaruli!");
+// console.log("its delicious");
+
+// window.alert("i really love adjaruli");
+
+//this is a comment in js
+
+/*this
+ is 
+ comment
+  too
+*/
+//a varriable is a container for storing data
+//a varriable behaves as if it was the value that it contains
+
+//2 steps
+//1. declaration (var,let,const)
+//2. Assigment (= assigment opereator)
+
+let age = 21;//Numbers
+//let age = "21"
+
+let firstname = "Daniel";//strings
+//age = age + 1;//age = 22
+let student = true;//booleans
+
+//age = age + "1"//=211 if let age = "21"
+
+console.log("hello",firstname);
+console.log("you are",age,"years old");
+console.log("enrolled",student);
+
+document.getElementById("p1").innerHTML = "hello " +  firstname;
+document.getElementById("p2").innerHTML = "you are " + age + " Years old";
+document.getElementById("p3").innerHTML =  "enrolled: " + student;
