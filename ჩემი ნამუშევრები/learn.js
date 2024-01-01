70db0d5d139ea1273ba650499c55630a6d2e078a
danielabramiani <danielabramiani4@gmail.com>
2024-01-01 17:42:03 +0400

    second commit

--- ჩემი ნამუშევრები/learn.js
+++ ჩემი ნამუშევრები/learn.js
@@ -17,19 +17,19 @@
 //1. declaration (var,let,const)
 //2. Assigment (= assigment opereator)
 
-let age = 21;//Numbers
-//let age = "21"
+// let age = 21;//Numbers
+// //let age = "21"
 
-let firstname = "Daniel";//strings
-//age = age + 1;//age = 22
-let student = true;//booleans
+// let firstname = "Daniel";//strings
+// //age = age + 1;//age = 22
+// let student = true;//booleans
 
-//age = age + "1"//=211 if let age = "21"
+// //age = age + "1"//=211 if let age = "21"
 
-console.log("hello",firstname);
-console.log("you are",age,"years old");
-console.log("enrolled",student);
+// console.log("hello",firstname);
+// console.log("you are",age,"years old");
+// console.log("enrolled",student);
 
-document.getElementById("p1").innerHTML = "hello " +  firstname;
-document.getElementById("p2").innerHTML = "you are " + age + " Years old";
-document.getElementById("p3").innerHTML =  "enrolled: " + student;
+// document.getElementById("p1").innerHTML = "hello " +  firstname;
+// document.getElementById("p2").innerHTML = "you are " + age + " Years old";
+// document.getElementById("p3").innerHTML =  "enrolled: " + student;
